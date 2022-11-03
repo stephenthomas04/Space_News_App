0a06effef209cbda087c0d179260d7be0b058950
Stephen Thomas <stephen.thomas042306@gmail.com>
2022-11-02 19:41:35 -0500

    11/2/22 @home
    added a hamburger menu with all the basic sections that we need. Will apply to main Space App so that I can connect it.

---
+++ app/src/main/java/com/example/hamburgertester/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.hamburgertester;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final DrawerLayout drawerLayout = findViewById(R.id.drawerLayout);
+
+        findViewById(R.id.imageMenu).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+    }
+}
