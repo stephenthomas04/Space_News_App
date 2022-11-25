0ad041e7aff2235f426fdffe2e75d74ab8a7301f
Stephen Thomas <stephen.thomas042306@gmail.com>
2022-11-25 14:59:43 -0600

    re added home screen

--- app/src/main/java/com/example/hamburgertester/home.java
+++ app/src/main/java/com/example/hamburgertester/home.java
@@ -1,28 +1,19 @@
 package com.example.hamburgertester;
 
-import android.content.Context;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import java.util.ArrayList;
-
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Home#newInstance} factory method to
+ * Use the {@link home#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Home extends Fragment {
-    Context thisContext;
+public class home extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -33,7 +24,7 @@ public class Home extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Home() {
+    public home() {
         // Required empty public constructor
     }
 
@@ -43,11 +34,11 @@ public class Home extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Home.
+     * @return A new instance of fragment home.
      */
     // TODO: Rename and change types and number of parameters
-    public static Home newInstance(String param1, String param2) {
-        Home fragment = new Home();
+    public static home newInstance(String param1, String param2) {
+        home fragment = new home();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -67,46 +58,7 @@ public class Home extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
         // Inflate the layout for this fragment
-        Drawable aash = ContextCompat.getDrawable(getActivity(), R.drawable.aashman_pic);
-        Drawable steph = ContextCompat.getDrawable(getActivity(), R.drawable.stephen_profilepic);
-        Drawable eng = ContextCompat.getDrawable(getActivity(), R.drawable.enguerranpfp);
-
-
-
-        ArrayList<TeamsObj> team = new ArrayList<>();
-
-        TeamsObj aashman = new TeamsObj(aash, "Aashman", getString(R.string.sampleDesc));
-        team.add(aashman);
-
-        TeamsObj Stephen = new TeamsObj(steph, "Stephen", getString(R.string.ST_desc));
-        team.add(Stephen);
-
-        TeamsObj Enguerran = new TeamsObj(eng, "Enguerran", getString(R.string.EP_desc));
-        team.add(Enguerran);
-
-
-
-
-
-
-        LinearLayoutManager layoutManager = new LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false);
-
-
-/*
-        RecyclerView recyclerView = getView().findViewById(R.id.teamRecycler);
-
-
-        Teams_RecyclerViewAdapter adapter = new Teams_RecyclerViewAdapter(thisContext, team);
-
-        recyclerView.setAdapter(adapter);
-        recyclerView.setLayoutManager(layoutManager);
-*/
-
         return inflater.inflate(R.layout.fragment_home, container, false);
-
     }
-
 }
